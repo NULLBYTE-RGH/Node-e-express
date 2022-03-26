bd34f7c2817549ce3ebe6c44fb15bb8afa504c03
joao <joaovitorsarti@outlook.com>
2022-03-26 12:41:56 -0300

    Test  Express

--- mss_lembretes/index.js
+++ mss_lembretes/index.js
@@ -1,18 +1,36 @@
+const { text } = require('express')
 const express = require('express')
-
 const app = express()
+//criamos um middleware
+//npm faker
+app.use(express.json())// possibilita o uso do req.body
+
+app.use((req,res,next)=>{
+    console.log("oi")
+    next()
+})
+
+////////////////
+
+const lembretes = {}
+var contador = 0
+
 
 //exemplo.com/lembretes
 //GET
 
 app.get('/lembretes', (req,res) => {
-
+    res.send(lembretes)
 })
 
 //POST
 
 app.post('/lembretes', (req,res) => {
-
+    contador++
+    //{texto: "fazer cafe"}
+    const { texto } = req.body
+    lembretes[contador] = {contador,texto}
+    res.status(201).send(lembretes[contador])
 })
 
 app.listen(4000, ()=> {
