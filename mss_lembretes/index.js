9e3ee3270c7a755526a761051cd73f33d7979443
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2022-04-02 13:08:07 -0300

    Add files via upload

--- mss_lembretes/index.js
+++ mss_lembretes/index.js
@@ -1,29 +1,48 @@
+const { text } = require('express')
 const express = require('express')
 const app = express()
-app.use(express.json)
+//criamos um middleware
+//npm faker
+app.use(express.json())// possibilita o uso do req.body
 
+app.use((req,res,next)=>{
+    console.log("oi")
+    next()
+})
+
+////////////////
 
 const lembretes = {}
-contador = 0
+var contador = 0
+
+
+//exemplo.com/lembretes
 //GET
-//exemplo.com.br/lembretes
-app.get('/lembretes', (req, res) => {
+
+app.get('/lembretes', (req,res) => {
     res.send(lembretes)
 })
- 
+
 //POST
-//exemplo.com.br/lembretes
-app.post('/lembretes', (req, res) => {
+
+app.post('/lembretes', (req,res) => {
     contador++
-    //{texto: "Fazer café"}
+    //{texto: "fazer cafe"}
     const { texto } = req.body
-    lembretes[contador] = {contador, texto}
+    lembretes[contador] = {contador,texto}
     res.status(201).send(lembretes[contador])
 })
 
+//:id diz que o valor sera recebido(placeholder)
 
+app.post('/lembretes/:id/observacoes', (req,res) => {
 
-app.listen(4000, () => {
-    console.log("Lembretes. Porta 4000.")
 })
 
+// app.listen(4000, ()=> {
+//     console.log('Lembretes, Porta 4000')
+// })
+
+app.listen(5000, ()=> {
+    console.log('Lembretes, Porta 5000')
+})
